ff18246c2ef4ebd3304fb5e10c92fde77cc2cc69
LukeJS <lukejsandler@gmail.com>
2017-09-12 18:56:50 +0100

    Subdomain-accessible session cookies

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Tekkify Auth'),
+    'name' => env('APP_NAME', 'TekkifyAuth'),
 
     /*
     |--------------------------------------------------------------------------
