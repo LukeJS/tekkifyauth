eabce89d4ebd65c4199d188d93dfc420f4e90ac3
LukeJS <lukejsandler@gmail.com>
2017-09-15 13:27:57 +0100

    fix class name for deleting expired verification codes command

--- app/Console/Commands/DeleteExpiredVerificationCodesCommand.php
+++ app/Console/Commands/DeleteExpiredVerificationCodesCommand.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use App\VerificationCode;
 use Illuminate\Console\Command;
 
-class DeleteExpiredVerificationCodes extends Command
+class DeleteExpiredVerificationCodesCommand extends Command
 {
     /**
      * The name and signature of the console command.
